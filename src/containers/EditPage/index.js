b3a703f3e212173060a5ba39574b986be16b068e
jcolot <julien.colot@gmail.com>
2022-07-14 12:33:32 +0200

    feat: add toast + refactoring

--- src/containers/EditPage/index.js
+++ src/containers/EditPage/index.js
@@ -4,9 +4,9 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { findIndex, get, map, isEmpty } from 'lodash';
 // Components
 import Button from '../../components/Button';
@@ -25,45 +25,49 @@ const FILE_RELATIONS = {
   product: [{ name: 'pictures', multiple: true }],
 };
 
-class EditPage extends React.Component {
-  state = {
-    didCheckErrors: false,
-    errors: [],
-    inititalData: {},
-    modifiedData: {},
-  };
+const EditPage = () => {
 
-  async componentDidMount() {
-    if (this.props.match.params.id !== 'create') {
-      const { match: { params } } = this.props
+  const params = useParams();
+  const navigate = useNavigate();
+  const [didCheckErrors, setDidCheckErrors] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [inititalData, setInitialData] = useState({});
+  const [modifiedData, setModifiedData] = useState({});
+
+  const title = params.id === 'create' ? `Create a new ${params.contentType}` : `Edit ${params.id}`;
+  const display = layout[params.contentType];
+
+  const getData = async () => {
+    if (params.id !== 'create') {
       const requestURL = `http://localhost:1337/${params.contentType}/${params.id}`;
       const data = await request(requestURL, { method: 'GET' });
 
-      this.setState({ inititalData: data, modifiedData: data });
+      setInitialData({ inititalData: data });
+      setModifiedData({ data });
     }
   }
 
+  useEffect(() => {
+    getData();
+  }, []);
+
+
   // Reset form to its inital value
-  cancel = () => this.setState({ modifiedData: this.state.inititalData });
+  const cancel = () => setModifiedData(inititalData);
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const name = e.target.name;
-
-    this.setState({
-      modifiedData: { ...this.state.modifiedData, [name]: e.target.value },
-    });
+    setModifiedData({ ...modifiedData, [name]: e.target.value });
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // TODO handle errors
-    const { match: { params } } = this.props;
-    const { modifiedData } = this.state;
     const body = Object.keys(modifiedData).reduce((acc, current) => {
       if (findIndex(FILE_RELATIONS[params.contentType], ['name', current]) === -1) {
         acc[current] = modifiedData[current];
       } else {
-        const alreadyUploadedFiles = modifiedData[current].filter(file =>  {
+        const alreadyUploadedFiles = modifiedData[current].filter(file => {
           if (file instanceof File === false) {
             return file;
           }
@@ -74,7 +78,7 @@ class EditPage extends React.Component {
     }, {});
 
     const method = params.id === 'create' ? 'POST' : 'PUT';
-    const requestURL =  params.id === 'create' ? `http://localhost:1337/${params.contentType}`: `http://localhost:1337/${params.contentType}/${params.id}`;
+    const requestURL = params.id === 'create' ? `http://localhost:1337/${params.contentType}` : `http://localhost:1337/${params.contentType}/${params.id}`;
     return request(requestURL, { method, body: body })
       .then(resp => {
         // Send the upload request for each added file
@@ -105,7 +109,7 @@ class EditPage extends React.Component {
               return request('http://localhost:1337/upload', { method: 'POST', body, headers: {} }, false)
                 .catch(err => {
                   console.log('error upload', err.response);
-              });
+                });
             }
           });
         }
@@ -116,54 +120,48 @@ class EditPage extends React.Component {
         //  TODO: Handle errors
       }).finally(() => {
         // TODO: make sure the redirection happens when all the files have been updated
-        this.props.history.push(`/${params.contentType}`);
+        navigate(`/${params.contentType}`);
       });
   }
 
-  render() {
-    const { match: { params } } = this.props;
-    const title = params.id === 'create' ? `Create a new ${params.contentType}` : `Edit ${params.id}`;
-    const display = layout[params.contentType];
-
-    return (
-      <div className="editPageWrapper">
-        <div className="container-fluid">
-          <h1>{title}</h1>
-          <Link to={`/${params.contentType}`}>Back</Link>
-          <form className="formWrapper" onSubmit={this.handleSubmit}>
-            <div className="row">
-              {display.map((input) => (
-                <Input
-                  didCheckErrors={this.state.didCheckErrors}
-                  errors={get(
-                    this.state.errors,
-                    [
-                      findIndex(this.state.errors, ['name', input.name]),
-                      'errors',
-                    ],
-                    []
-                  )}
-                  key={input.name}
-                  label={input.name}
-                  name={input.name}
-                  onChange={this.handleChange}
-                  type={input.type}
-                  value={get(this.state.modifiedData, input.name, input.type === 'file' ? [] : '')}
-                  multiple
-                />
-              ))}
+  return (
+    <div className="editPageWrapper">
+      <div className="container-fluid">
+        <h1>{title}</h1>
+        <Link to={`/${params.contentType}`}>Back</Link>
+        <form className="formWrapper" onSubmit={handleSubmit}>
+          <div className="row">
+            {display.map((input) => (
+              <Input
+                didCheckErrors={didCheckErrors}
+                errors={get(
+                  errors,
+                  [
+                    findIndex(errors, ['name', input.name]),
+                    'errors',
+                  ],
+                  []
+                )}
+                key={input.name}
+                label={input.name}
+                name={input.name}
+                onChange={handleChange}
+                type={input.type}
+                value={get(modifiedData, input.name, input.type === 'file' ? [] : '')}
+                multiple
+              />
+            ))}
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <Button type="button" onClick={cancel}>Cancel</Button>
+              <Button type="submit" primary>Submit</Button>
             </div>
-            <div className="row">
-              <div className="col-md-12">
-                <Button type="button" onClick={this.cancel}>Cancel</Button>
-                <Button type="submit" primary>Submit</Button>
-              </div>
-            </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 EditPage.defaultProps = {};
